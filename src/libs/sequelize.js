3f127599d8e1fc1e507f4d0e650df2cf192104e2
Andrés Cifuentes <andrescf@outlook.com>
2024-04-11 19:18:41 -0500

    cambion de db

--- src/libs/sequelize.js
+++ src/libs/sequelize.js
@@ -3,16 +3,10 @@ import setupModel from '../db/setupModels.js';
 
 const USER = encodeURIComponent(process.env.DB_USER);
 const PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
-const URI = `mysql://${USER}:${PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?sslmode=no-verify`;
+const URI = `mysql://${USER}:${PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`;
 const sequelize = new Sequelize(URI, {
   dialect: 'mysql',
   logging: false,
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: true,
-    },
-  },
-  constraints: false,
 });
 
 setupModel(sequelize);
